8e3dcbfa97ce7c28e5940527897de2958fc1e996
Kangkang-wky <wky_bayonet@163.com>
2024-01-12 14:54:56 +0000

    backward acc

--- my_train.sh
+++ my_train.sh
@@ -7,3 +7,10 @@ python train.py \
     -m ./output/truck_improve/  \
     --iterations 30000 \
     > train.log 2>&1
+
+# nsys profile -w true -t cuda,nvtx,osrt,cublas --cuda-memory-usage=true --cudabacktrace=true -x true -f true -o rep_train_500 \
+# python train.py \
+#     -s ./data/$DATA \
+#     -m ./output/truck_improve/  \
+#     --iterations 500 \
+#     > train.log 2>&1
