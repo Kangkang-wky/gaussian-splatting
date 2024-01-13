6f80e7076c698a2c01cb261a03bd8f4f37f46e17
Kangkang-wky <wky_bayonet@163.com>
2024-01-13 13:26:25 +0000

    pipeline buffer

--- my_train.sh
+++ my_train.sh
@@ -2,11 +2,11 @@
 
 DATA=tandt/truck/
 
-# python train.py \
-#     -s ./data/$DATA \
-#     -m ./output/truck_improve/  \
-#     --iterations 30000 \
-#     > train.log 2>&1
+python train.py \
+    -s ./data/$DATA \
+    -m ./output/truck_improve/  \
+    --iterations 30000 \
+    > train.log 2>&1
 
 # python train.py \
 #     -s ./data/$DATA \
@@ -14,13 +14,13 @@ DATA=tandt/truck/
 #     --checkpoint_iterations 30000 \
 #     > train.log 2>&1
 
-nsys profile -w true -t cuda,nvtx,osrt,cublas --cuda-memory-usage=true --cudabacktrace=true -x true -f true -o rep_train_30100 \
-python train.py \
-    -s ./data/$DATA \
-    -m ./output/truck_improve/  \
-    --start_checkpoint /home/wangkangyu/gaussian_workspace/my/gaussian-splatting/output/truck_improve/chkpnt30000.pth  \
-    --iterations 30100 \
-    > train_truck_me.log 2>&1
+# nsys profile -w true -t cuda,nvtx,osrt,cublas --cuda-memory-usage=true --cudabacktrace=true -x true -f true -o rep_train_30100 \
+# python train.py \
+#     -s ./data/$DATA \
+#     -m ./output/truck_improve/  \
+#     --start_checkpoint /home/wangkangyu/gaussian_workspace/my/gaussian-splatting/output/truck_improve/chkpnt30000.pth  \
+#     --iterations 30100 \
+#     > train_truck_me.log 2>&1
 
 
 # nsys profile -w true -t cuda,nvtx,osrt,cublas --cuda-memory-usage=true --cudabacktrace=true -x true -f true -o rep_train_500 \
