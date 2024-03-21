c3043a95808defa95ad43fdb6a6759f6d50fe3ab
Kangkang-wky <wky_bayonet@163.com>
2024-03-21 13:42:28 +0000

    check parameter

--- my_render.sh
+++ my_render.sh
@@ -2,20 +2,18 @@
 
 DATA=tandt/truck/
 
-# python render.py \
-#     -s ./data/$DATA \
-#     -m ./output/truck_improve/ \
-#     -r 1  \
-#     > render.log 2>&1
-
-
-
-nsys profile -w true -t cuda,nvtx,osrt,cublas --cuda-memory-usage=true --cudabacktrace=true -x true -f true -o rep_renderCUDA_improve \
-/home/wangkangyu/miniconda3/envs/gaussian_splatting/bin/python render.py \
+python render.py \
     -s ./data/$DATA \
     -m ./output/truck_improve/ \
     -r 1  \
     > render.log 2>&1
+
+# nsys profile -w true -t cuda,nvtx,osrt,cublas --cuda-memory-usage=true --cudabacktrace=true -x true -f true -o rep_renderCUDA_improve \
+# /home/wangkangyu/miniconda3/envs/gaussian_splatting/bin/python render.py \
+#     -s ./data/$DATA \
+#     -m ./output/truck_improve/ \
+#     -r 1  \
+#     > render.log 2>&1
 
 # sudo /home/wangkangyu/cuda_version/usr/local/cuda-11.7.0/bin/ncu --set full --kernel-name renderCUDA -o rep_kernel_renderCUDA \
 # /home/wangkangyu/miniconda3/envs/gaussian_splatting/bin/python render.py \
